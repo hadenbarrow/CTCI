78a1b15ff0e2d41788d0f53c98263806f70c538c
Haden Barrow <hadenbarrow@csus.edu>
2020-07-09 01:51:22 -0700

    Solved 1.1b

--- Practice Problems/src/arraysAndStrings/Answers.java
+++ Practice Problems/src/arraysAndStrings/Answers.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 
 public class Answers {
 	
-	//1.1
+	//1.1 a
 	public boolean hasDupeChars(String input) {
 		HashSet<Character> seen = new HashSet<Character>();
 		for(int i = 0; i < input.length(); i++) {
@@ -17,15 +17,44 @@ public class Answers {
 		return false;
 	}
 	
+	//1.1 b - What if you can't use an additional data structure?
+	public boolean hasDupeChars2(String input) {
+		for(int i = 0; i < input.length(); i++) {
+			for(int j = 0; j < input.length(); j++) {
+				if(input.charAt(i) == input.charAt(j) && i != j) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
 	public static void main(String[] args) {
 		Answers answers = new Answers();
 		
-		//1.1 tests
-		String noDuplicates = "no dupes";
-		String hasDuplicates = "has dupess";
 		
+		String noDuplicates = "no dupes"; //false
+		String hasDuplicates = "has dupess"; //true
+		String edgeCase1 = ""; //false
+		String edgeCase2 = "1"; //false
+		String edgeCase3 = "1122"; //true
+		
+		//1.1a tests
+		System.out.println("---1.1a tests---");
 		System.out.println(answers.hasDupeChars(noDuplicates));
 		System.out.println(answers.hasDupeChars(hasDuplicates));
+		System.out.println(answers.hasDupeChars(edgeCase1));
+		System.out.println(answers.hasDupeChars(edgeCase2));
+		System.out.println(answers.hasDupeChars(edgeCase3));
+		
+		//1.1b tests
+		System.out.println("---1.1b tests---");
+		System.out.println(answers.hasDupeChars2(noDuplicates));
+		System.out.println(answers.hasDupeChars2(hasDuplicates));
+		System.out.println(answers.hasDupeChars2(edgeCase1));
+		System.out.println(answers.hasDupeChars2(edgeCase2));
+		System.out.println(answers.hasDupeChars2(edgeCase3));
+		
 		
 	}
 	
